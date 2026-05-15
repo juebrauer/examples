9fe973c87d8573607b0b48d9f770cec047109f9b
juebrauer <juebrauer@gmail.com>
2026-05-15 11:15:11 +0200

    First ePuck robot controller with explorative behavior and obstacle avoidance

--- 037_webots/tutorial1/controllers/ctrl1/ctrl1.py
+++ 037_webots/tutorial1/controllers/ctrl1/ctrl1.py
@@ -1,5 +1,7 @@
 from controller import Robot, Motor
 
+print("Controller v7 - simple obstacle avoidance with state machine")
+
 TIME_STEP = 64
 
 MAX_SPEED = 6.28
@@ -14,8 +16,42 @@ leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
 
 # set up the motor speeds at 10% of the MAX_SPEED.
-leftMotor.setVelocity(0.2 * MAX_SPEED)
-rightMotor.setVelocity(0.1 * MAX_SPEED)
+leftMotor.setVelocity(0 * MAX_SPEED)
+rightMotor.setVelocity(0 * MAX_SPEED)
+
+# get handlers to the 8 IR sensors and enable them
+ir_sensors = []
+ir_sensor_names = [
+    'ps0', 'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6', 'ps7'
+]
+for name in ir_sensor_names:
+    sensor = robot.getDevice(name)
+    sensor.enable(TIME_STEP)
+    ir_sensors.append(sensor)
+
+state = "FORWARD"
 
 while robot.step(TIME_STEP) != -1:
-   pass
+   # read values from the IR sensors
+   ir_vec = []
+   ir_values = ""
+   for sensor in ir_sensors:
+      value = sensor.getValue()
+      ir_values += f"{value:.2f} "
+      ir_vec.append(value)
+   print(f"{state} : {ir_values}")
+
+   # simple obstacle avoidance logic
+   if state == "FORWARD":
+      leftMotor.setVelocity(0.5 * MAX_SPEED)
+      rightMotor.setVelocity(0.5 * MAX_SPEED)
+      if ir_vec[0] > 80 or ir_vec[7] > 80 or ir_vec[1] > 80 or ir_vec[6] > 80:
+         state = "TURN"
+   elif state == "TURN":
+      leftMotor.setVelocity(0.5 * MAX_SPEED)
+      rightMotor.setVelocity(-0.5 * MAX_SPEED)
+      if ir_vec[0] < 80 and ir_vec[7] < 80 and ir_vec[1] < 80 and ir_vec[6] < 80:
+         state = "FORWARD"
+
+   
+   
